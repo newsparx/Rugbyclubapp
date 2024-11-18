b71ab4e6eafd7f6f1f830a3f42bac79fc440ec71
newsparx <78025478+newsparx@users.noreply.github.com>
2024-11-18 01:15:37 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignInPortal from './components/SignInPortal';
+
+function App() {
+  return (
+    <div>
+      <SignInPortal />
+    </div>
+  );
+}
+
+export default App;
